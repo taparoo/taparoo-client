e2f5dece4a4bf6b33a65c535845675f6f4dc1b01
Austin <joha0033@gmail.com>
2017-08-04 12:46:15 -0600

    get is working for inventory

--- app/app.module.js
+++ app/app.module.js
@@ -2,6 +2,20 @@
   'use strict'
   //just defining the module
     angular
-      .module('taparoo', [])
-
+      .module('taparoo', ['ui.router'])
+      .component('tapDisplay', {
+        controller: 'tapDisplayController',
+        templateUrl: 'tap-display/tap-display.html'
+      })
+      .component('selectBeers', {
+        controller: 'tapSelectController',
+        templateUrl: 'tap-display/tap-select/tap-select.html',
+        bindings: {
+            data: '<'
+          }
+      })
+      // .component('tapDisplay', {
+      //   controller: 'tapDisplayController',
+      //   templateUrl: 'tap-disaply/tap-select/tap-display.html'
+      // })
 }());
---
+++ app/tap-display/tap-select/tap-select.controller.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('taparoo')
+    .controller('tapSelectController', tapSelectController)
+
+    function tapSelectController($http, $stateParams, $state){
+      const vm = this
+
+      $http.get('https://taparoo-server.herokuapp.com/api/v1/beers').then(function(res){
+        console.log(res.data.beers["0"].name);
+        vm.beerAPI = res.data.beers
+        vm.dropdownOptions ={}
+        //refactor wo HO Function
+        for(let i=0;i<res.data.beers.length; i++){
+          // console.log(res[i].name);
+          vm.dropdownOptions[res.data.beers[i].name]=res.data.beers[i].name
+        }
+        } )
+
+    vm.addBeer = function(){
+      vm.tapped1 = [];
+      vm.tapped2 = [];
+      vm.cooler = [];
+      for(let i=0;i<vm.beerAPI.length;i++){
+        if(vm.beer1.name === vm.beerAPI[i].name){
+          console.log(vm.beerAPI[i]);
+            vm.tapped1.push(vm.beerAPI[i])
+        }
+        if(vm.beer2.name === vm.beerAPI[i].name){
+          console.log(vm.beerAPI[i]);
+            vm.tapped2.push(vm.beerAPI[i])
+        }
+        if(vm.cooler.name === vm.beerAPI[i].name){
+          console.log(vm.beerAPI[i]);
+            vm.tapped3.push(vm.beerAPI[i])
+        }
+      }
+      //$state.go('/tap-display')
+    }
+  }
+}());
