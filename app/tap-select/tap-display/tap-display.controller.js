0a5b92ba0f3766cebb7d093e1cf5f8cf68e86583
Michael Balagna <lunchmeat13@gmail.com>
2017-08-04 14:38:04 -0600

    add to tap-display controller. Changed tap-display html as well. Better looking cards.

--- app/tap-select/tap-display/tap-display.controller.js
+++ app/tap-select/tap-display/tap-display.controller.js
@@ -8,9 +8,15 @@
 
     controller.$inject = ['$http']
     function tapDisplayController($http){
+      const vm = this;
       console.log('hellow frome tha tapDisplay controller');
-      $http.get('https://taparoo-server.herokuapp.com/api/v1/beers').then(function(res){
-        console.log(res)
+      $http.get('https://taparoo-server.herokuapp.com/api/v1/beers/on_tap').then(function(res){
+        console.log(res.data["0"]);
+        vm.tapped1 = [];
+        vm.tapped2 = [];
+        vm.tapped1.push(res.data["0"]);
+        vm.tapped2.push(res.data["1"]);
+        console.log(vm.tapped1);
       } )
 
     }}());
